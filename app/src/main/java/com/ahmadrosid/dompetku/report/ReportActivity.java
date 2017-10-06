c0a52bc16a829438e08d305ad06784bbd042781f
bashir arrohman <tyangjawi03@gmail.com>
2017-10-07 06:14:29 +0700

    add report activity

---
+++ app/src/main/java/com/ahmadrosid/dompetku/report/ReportActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadrosid.dompetku.report;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ahmadrosid.dompetku.R;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+
+}
