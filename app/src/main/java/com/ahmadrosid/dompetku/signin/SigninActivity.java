0eb834bd988ecbf9f0aa6a3b512c0777d55af951
Bashir Arrohman <bashir.arrohman@bssn.go.id>
2018-04-10 16:04:50 +0700

    add signin activity

---
+++ app/src/main/java/com/ahmadrosid/dompetku/signin/SigninActivity.java
@@ -0,0 +1,112 @@
+package com.ahmadrosid.dompetku.signin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ahmadrosid.dompetku.R;
+import com.ahmadrosid.dompetku.main.MainActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SigninActivity extends AppCompatActivity {
+
+    private String TAG = SigninActivity.class.getSimpleName();
+
+    @BindView(R.id.google_signin_button)
+    Button googleSigninButton;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private int RC_SIGN_IN = 7001;
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, SigninActivity.class);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        ButterKnife.bind(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    @OnClick(R.id.google_signin_button)
+    public void onViewClicked() {
+
+        signIn();
+
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            Toast.makeText(this, account.getEmail(), Toast.LENGTH_SHORT).show();
+            //TODO : already signin
+
+            MainActivity.start(this);
+            finish();
+        } else {
+            Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode() + " " +e.getMessage());
+            updateUI(null);
+        }
+    }
+
+}
