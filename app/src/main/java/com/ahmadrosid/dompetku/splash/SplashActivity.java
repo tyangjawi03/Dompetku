1b5400778ac0414d7d86632b90b8b3d55cf5dc80
Bashir Arrohman <bashir.arrohman@bssn.go.id>
2018-04-11 14:35:53 +0700

    add splash activity

---
+++ app/src/main/java/com/ahmadrosid/dompetku/splash/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.ahmadrosid.dompetku.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
